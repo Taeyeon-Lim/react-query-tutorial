f6f5d1bc8b517dd66808880d54171c9db1f8fc9c
Teayeon-Lim <wingofyeon@naver.com>
2021-11-24 20:02:20 +0900

    Mutation 훅 사용해보기

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { useUserList } from '../hooks/useUserList';
+import React, { useState } from 'react';
+import { useAddUser, useUserList } from '../hooks/useUserList';
 import UserItem from './UserItem';
 
 function UserList() {
+  const [name, setName] = useState('');
+  const [nickname, setNickName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const onChangeName = e => setName(e.target.value);
+  const onChangeEmail = e => setEmail(e.target.value);
+  const onChangeNickName = e => setNickName(e.target.value);
+
   const onSuccess = data => {
     console.log(
       '데이터를 가져온 다음에 실행되는 사이드 이펙트를 수행합니다',
       data
     );
   };
+
   const onError = error => {
     console.log(
       '데이터 페칭이 실패한 다음에 실행되는 사이드 이펙트를 수행합니다',
@@ -24,11 +33,42 @@ function UserList() {
     refetch,
   } = useUserList(onSuccess, onError);
 
+  const { mutate: addUser } = useAddUser();
+
+  const onAddUser = () => {
+    const user = { name, username: nickname, email };
+    console.log(user);
+    addUser(user);
+  };
+
   if (isLoading || isFetching) return <div>로딩 중...</div>;
   if (isError) return <div>에러...!</div>;
 
   return (
     <>
+      <div>
+        <input
+          type='text'
+          placeholder='이름을 입력하세요.'
+          value={name}
+          onChange={onChangeName}
+        />
+        <input
+          type='text'
+          placeholder='닉네임을 입력하세요.'
+          value={nickname}
+          onChange={onChangeNickName}
+        />
+        <input
+          type='text'
+          placeholder='이메일을 입력하세요.'
+          value={email}
+          onChange={onChangeEmail}
+        />
+        <button type='button' onClick={onAddUser}>
+          User 추가
+        </button>
+      </div>
       <button onClick={refetch}>데이터 가져오기</button>
       <ul>
         {users?.map(user => (
--- src/hooks/useUserList.js
+++ src/hooks/useUserList.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import { useQuery } from 'react-query';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 
-const fetchUserList = () => {
-  return axios.get('https://jsonplaceholder.typicode.com/users');
-};
+const fetchUserList = () => axios.get('http://localhost:4000/users');
+
+const addUser = user => axios.post('http://localhost:4000/users', user);
 
 export const useUserList = (onSuccess, onError) => {
   return useQuery('users', fetchUserList, {
@@ -16,6 +16,19 @@ export const useUserList = (onSuccess, onError) => {
       }, []);
       return newArrData;
     },
-    enabled: false,
+    // enabled: false,
+  });
+};
+
+export const useAddUser = () => {
+  const queryClient = useQueryClient();
+
+  // useMutation 은 반드시 키를 필요로 하지 않습니다.
+  return useMutation(addUser, {
+    onSuccess: () => {
+      // 쿼리 클라리언트의 무효화 쿼리를 'users"인 쿼리 키에 사용합니다.
+      // 따라서, 기존 "users"로 호출된 쿼리는 무효화되고 다시 가져옵니다.
+      queryClient.invalidateQueries('users');
+    },
   });
 };
